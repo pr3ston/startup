7807785e40d06dc122409ff29fc1f5e6189edbd5
Preston <viloriapreston@gmail.com>
2026-02-10 23:21:07 -0700

    working on injected routed component, but not working with routes

--- simon-react-p1/src/app.jsx
+++ simon-react-p1/src/app.jsx
@@ -1,50 +1,57 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+// import { Login } from './login/login';
+// import { Play } from './play/play';
+// import { Scores } from './scores/scores';
+// import { About } from './about/about';
 
 export default function App() {
   return (
-    <div className="body bg-dark text-light">
-      <header className="container-fluid">
-        <nav className="navbar fixed-top navbar-dark">
-          <div className="navbar-brand">
-            Simon<sup>&reg;</sup>
-          </div>
-          <menu className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="index.html">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="play.html">
-                Play
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="scores.html">
-                Scores
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </menu>
-        </nav>
-      </header>
+    <BrowserRouter>
+        <div className="body bg-dark text-light">
+        <header className="container-fluid">
+            <nav className="navbar fixed-top navbar-dark">
+  <div className="navbar-brand">
+    Simon<sup>&reg;</sup>
+  </div>
+  <menu className="navbar-nav">
+    <li className="nav-item">
+      <NavLink className="nav-link" to="">
+        Login
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink className="nav-link" to="play">
+        Play
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink className="nav-link" to="scores">
+        Scores
+      </NavLink>
+    </li>
+    <li className="nav-item">
+      <NavLink className="nav-link" to="about">
+        About
+      </NavLink>
+    </li>
+  </menu>
+</nav>
+        </header>
 
-      <main>App components go here</main>
+        <main>App components go here</main>
 
-      <footer className="bg-dark text-white-50">
-        <div className="container-fluid">
-          <span className="text-reset">Author Name(s)</span>
-          <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
-            Source
-          </a>
+        <footer className="bg-dark text-white-50">
+            <div className="container-fluid">
+            <span className="text-reset">Author Name(s)</span>
+            <a className="text-reset" href="https://github.com/webprogramming260/simon-react">
+                Source
+            </a>
+            </div>
+        </footer>
         </div>
-      </footer>
-    </div>
+    </BrowserRouter>
   );
 }
