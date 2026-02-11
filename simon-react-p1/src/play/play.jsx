6c82a3357f89d7c583c6cbb4290978ee8d0819a0
Preston Viloria <viloriapreston@gmail.com>
2026-02-10 23:54:50 -0700

    finished routed component part; problem was the function names that were being exported

--- simon-react-p1/src/play/play.jsx
+++ simon-react-p1/src/play/play.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export function Login() {
+export function Play() {
   return (
     <main className="container-fluid bg-secondary text-center">
       <div>Play displayed here</div>
--- simon-react-p1/src/scores/scores.jsx
+++ simon-react-p1/src/scores/scores.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-export function Login() {
+export function Scores() {
   return (
     <main className="container-fluid bg-secondary text-center">
       <div>Scores displayed here</div>
