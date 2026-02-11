fba04cd8288f957accdbe805e54393d00fa0de84
Preston Viloria <viloriapreston@gmail.com>
2026-02-11 15:58:23 -0700

    added div to the register component which fixed the formatting

--- src/register/register.jsx
+++ src/register/register.jsx
@@ -4,32 +4,34 @@ import "../../index-styles.css";
 export function Register() {
   return (
     <main>
-      <form method="post" action="channels.html">
-        <h1>Register for Yapp</h1>
-        <div class="mb-3">
-          <label for="formGroupExampleInput" class="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="formGroupExampleInput"
-            placeholder="Username"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="formGroupExampleInput2" class="form-label">
-            Password
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="formGroupExampleInput2"
-            placeholder="Password"
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      <div class="form-div">
+        <form method="post" action="channels.html">
+          <h1>Register for Yapp</h1>
+          <div class="mb-3">
+            <label for="formGroupExampleInput" class="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput"
+              placeholder="Username"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="formGroupExampleInput2" class="form-label">
+              Password
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput2"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit">Register</button>
+        </form>
+      </div>
     </main>
   );
 }
