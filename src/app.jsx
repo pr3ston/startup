23cd05dd05276bccec5f76f828fb6a511ca8ad3a
Preston Viloria <viloriapreston@gmail.com>
2026-02-11 11:37:39 -0700

    finished adding the routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,48 +1,69 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "/index-styles.css";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Register } from "./register/register";
+import { Channels } from "./channels/channels";
+import { About } from "./about/about";
 
 export default function App() {
   return (
-    <div>
-      <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li>
-              <a class="navbar-brand">Yapp</a>
-            </li>
-            <li>
-              <a class="nav-link" href="index.html">
-                Login
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="register.html">
-                Register
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="channels.html">
-                Channels
-              </a>
-            </li>
-            <li>
-              <a class="nav-link" href="about.html">
-                About
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+    <BrowserRouter>
+      <div>
+        <header>
+          <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li>
+                <a class="navbar-brand">Yapp</a>
+              </li>
+              <li>
+                <NavLink className="nav-link" to="/">
+                  Login
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-link" to="/register">
+                  Register
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-link" to="/channels">
+                  Channels
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="nav-link" to="/about">
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
 
-      <main>This is the main page</main>
+        <Routes>
+          <Route path="/" element={<Login />} exact />
+          <Route path="/register" element={<Register />} />
+          <Route path="/channels" element={<Channels />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
 
-      <footer>
-        <hr />
-        <span class="text-reset">Author Name(s): Preston Viloria</span>
-        <br />
-        <a href="https://github.com/pr3ston/startup/tree/main">GitHub</a>
-      </footer>
-    </div>
+        <footer>
+          <hr />
+          <span class="text-reset">Author Name(s): Preston Viloria</span>
+          <br />
+          <a href="https://github.com/pr3ston/startup/tree/main">GitHub</a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
   );
 }
