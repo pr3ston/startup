d85f80046b3f2ff5e9354ed298fcab522c371fb3
Preston Viloria <viloriapreston@gmail.com>
2026-02-23 21:33:09 -0700

    finished app.jsx again

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "/index-styles.css";
-// import "./channels/channel-styles.css";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { Login } from "./login/login";
 import { Register } from "./register/register";
