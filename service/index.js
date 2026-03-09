acf850be799ffb48d7280f9dd0fc61894f8ede2a
Preston Viloria <viloriapreston@gmail.com>
2026-03-09 14:16:11 -0600

    finished initial setup

---
+++ service/index.js
@@ -0,0 +1,9 @@
+const cookieParser = require("cookie-parser");
+const bcrypt = require("bcryptjs");
+const express = require("express");
+const uuid = require("uuid");
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.static("public"));
