595baeed347fa3c5852bafa0ea78c1787daae35e
Preston Viloria <viloriapreston@gmail.com>
2026-03-09 14:47:59 -0600

    added login, auth, and logout endpoints

--- service/index.js
+++ service/index.js
@@ -4,6 +4,90 @@ const express = require("express");
 const uuid = require("uuid");
 const app = express();
 
+const authCookieName = "token";
+
+let users = [];
+
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.json());
+
+app.use(cookieParser());
+
 app.use(express.static("public"));
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+apiRouter.post("/auth/create", async (req, res) => {
+  if (await findUser("email", req.body.email)) {
+    res.status(409).send({ msg: "Existing user" });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post("/auth/login", async (req, res) => {
+  const user = await findUser("email", req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: "Unauthorized" });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete("/auth/logout", async (req, res) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser("token", req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: "Unauthorized" });
+  }
+};
+
+async function createUser(email, password) {
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  users.push(user);
+
+  return user;
+}
+
+async function findUser(field, value) {
+  if (!value) return null;
+
+  return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+  res.cookie(authCookieName, authToken, {
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+  });
+}
